149d895f509a95f9ef5defcd5ef67122d7274e33
DoItNowJessica <jesjon87@gmail.com>
2022-02-21 19:47:02 -0500

    Adding file

---
+++ CYOA.py
@@ -0,0 +1,7 @@
+#This is a choose your own adventure game
+
+print('Hello, adventurer. What is your name?')
+name = input()
+
+print('' + name + ', I have a quest for you. Will you accept? Yes or no?')
+answer = input()
