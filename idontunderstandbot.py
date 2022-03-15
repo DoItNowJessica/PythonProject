54167a77b10f7c47730a1215588f1c4373e9cb61
DoItNowJessica <jesjon87@gmail.com>
2022-03-14 20:10:35 -0400

    Why isn't it working

--- idontunderstandbot.py
+++ idontunderstandbot.py
@@ -1,1 +1,25 @@
-##This is a bot for discord that will look up an Urban Dictionary definition. 
+##This is a bot for discord that will look up an Urban Dictionary definition.
+
+import discord
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv("/c/Users/jesjo/Documents/.env")
+TOKEN = os.getenv('TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.getenv('TOKEN'))
